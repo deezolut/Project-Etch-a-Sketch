86744c22483eac41d222ec146ef2bb83ddd9de4e
deezolut <deezolut@gmail.com>
2024-10-15 22:10:16 +0300

    Replace some css with js
    This is to make the page more dynamic based on user's actions

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-let elementsCount = 16*16;
 const container = document.querySelector('#container');
+const elementsCount = 16*16;
+let num = 62/10;
 
 for (let i = 0; i < elementsCount; i++) {
     const newDiv = document.createElement('div');
-    newDiv.classList.add('gridBox')
+    newDiv.classList.add('gridBox');
+    newDiv.style.flex = `1 0 calc(${num}% - 2px)`;
     container.appendChild(newDiv); 
 }
